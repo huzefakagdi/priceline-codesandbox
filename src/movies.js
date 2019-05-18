5cfa5f1af800e49931c63b0841463c28da10edb6
Huzefa Kagdi <kahn_huzefa@yahoo.co.in>
2019-05-18 15:41:07 -0400

    Add movie file

---
+++ src/movies.js
@@ -0,0 +1,62 @@
+const movies = [
+  {
+    name: 'The Dark Knight',
+    rating: 'PG-13',
+    year: 2012,
+    bestScene: {
+      name: 'fight',
+      location: 'sewer',
+      sceneLength: 10,
+      actors: ['Christian Bale', 'Tom Hardy']
+    }
+  },
+  {
+    name: 'Good Burger',
+    rating: 'PG',
+    year: 1994,
+    bestScene: {
+      name: 'jump',
+      location: 'giant milkshake',
+      sceneLength: 5,
+      actors: ['Kenan Thompson', 'Kel Mitchell']
+    }
+  },
+  {
+    name: 'Sharknado 2',
+    rating: 'TV-14',
+    year: 2013
+  },
+  {
+    name: 'The Big Short',
+    rating: 'R',
+    year: 2015,
+    bestScene: {
+      name: 'explanantion',
+      location: 'casino',
+      sceneLength: 20,
+      actors: ['Christian Bale', 'Steve Carrell']
+    }
+  },
+];
+
+
+// return the total length of all of the best scenes in the movies
+function bestSceneCount(movies) {
+  
+  return movies;
+}
+
+const onlytheBestScenes = bestSceneCount(movies);
+console.log(`The best scenes are: ${onlytheBestScenes}`);
+
+// find and return an array of objects that have completely unique actors
+// (i.e. if an actor is in more than one movie, that movie is removed)
+function getMoviesForUniqueActors(movies) {
+
+  return movies;
+}
+
+function getUniqueActors(movies) {
+  
+  return movies;
+}
