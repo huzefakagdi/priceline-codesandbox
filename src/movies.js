06c6a0e785b4b83575311e2907d6a554ea198757
Huzefa Kagdi <kahn_huzefa@yahoo.co.in>
2019-05-18 16:33:06 -0400

    movies

--- src/movies.js
+++ src/movies.js
@@ -1,62 +1,68 @@
 const movies = [
   {
-    name: 'The Dark Knight',
-    rating: 'PG-13',
+    name: "The Dark Knight",
+    rating: "PG-13",
     year: 2012,
     bestScene: {
-      name: 'fight',
-      location: 'sewer',
+      name: "fight",
+      location: "sewer",
       sceneLength: 10,
-      actors: ['Christian Bale', 'Tom Hardy']
+      actors: ["Christian Bale", "Tom Hardy"]
     }
   },
   {
-    name: 'Good Burger',
-    rating: 'PG',
+    name: "Good Burger",
+    rating: "PG",
     year: 1994,
     bestScene: {
-      name: 'jump',
-      location: 'giant milkshake',
+      name: "jump",
+      location: "giant milkshake",
       sceneLength: 5,
-      actors: ['Kenan Thompson', 'Kel Mitchell']
+      actors: ["Kenan Thompson", "Kel Mitchell"]
     }
   },
   {
-    name: 'Sharknado 2',
-    rating: 'TV-14',
+    name: "Sharknado 2",
+    rating: "TV-14",
     year: 2013
   },
   {
-    name: 'The Big Short',
-    rating: 'R',
+    name: "The Big Short",
+    rating: "R",
     year: 2015,
     bestScene: {
-      name: 'explanantion',
-      location: 'casino',
+      name: "explanantion",
+      location: "casino",
       sceneLength: 20,
-      actors: ['Christian Bale', 'Steve Carrell']
+      actors: ["Christian Bale", "Steve Carrell"]
     }
-  },
+  }
 ];
 
-
 // return the total length of all of the best scenes in the movies
-function bestSceneCount(movies) {
-  
-  return movies;
+function bestScenes(movies) {
+  return movies.reduce((accumulator, movie) => {
+		const bestScene = movie.bestScene;
+		if (bestScene) {
+			accumulator.push(bestScene);
+    }
+    return accumulator;
+	}, []);
 }
 
-const onlytheBestScenes = bestSceneCount(movies);
+const onlytheBestScenes = bestScenes(movies);
 console.log(`The best scenes are: ${onlytheBestScenes}`);
 
-// find and return an array of objects that have completely unique actors
-// (i.e. if an actor is in more than one movie, that movie is removed)
-function getMoviesForUniqueActors(movies) {
-
-  return movies;
-}
-
 function getUniqueActors(movies) {
-  
-  return movies;
+  const bestSceneList = bestScenes(movies);
+  const actors = bestSceneList.reduce((accumulator, bestScene) => {
+    const actors = bestScene.actors;
+		if (actors && actors.length > 0) {
+			accumulator.push(...actors);
+    }
+    return accumulator;
+  }, []);
+  return [...new Set(actors)]; 
 }
+
+console.log(`The list of unique actors are: ${getUniqueActors(movies)}`);
